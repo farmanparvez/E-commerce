e03a81bcef45ab3b0b5cb6a917cba4c14f6ffec6
farman <farman@kmindz.in>
2022-10-07 19:50:29 +0530

    fixed

--- app.js
+++ app.js
@@ -40,10 +40,10 @@ app.use("/api", productRouter);
 app.use("/api/order", orderRouter);
 app.use("/api/uploads", uploadRouter);
 
-// app.use(`/uploads`, express.static(`uploads`));
+app.use(`/uploads`, express.static(`uploads`));
 // app.use(`/uploads`, express.static(`uploads`));
 // app.use(express.static(path.join(__dirname, './uploads')));
-app.use(express.static(path.resolve(__dirname, 'uploads')));
+// app.use(express.static(path.resolve(__dirname, 'uploads')));
 
 if (process.env.NODE_ENV === "production") {
   // app.use(express.static("frontend/build"));
