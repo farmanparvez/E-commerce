32ef76e54505283fdac27dbe201c208d2c4a2a01
farman <farman@kmindz.in>
2022-10-07 19:26:36 +0530

    fixed

--- app.js
+++ app.js
@@ -44,7 +44,8 @@ app.use(`/uploads`, express.static(`uploads`));
 // app.use(express.static(path.join(__dirname, './uploads')));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
+  // app.use(express.static("frontend/build"));
+  app.use(express.static(path.resolve(__dirname, "frontend", "build")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
