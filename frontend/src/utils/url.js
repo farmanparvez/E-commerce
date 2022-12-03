8808c6a04721a76d4ff704faeec924fa1ef502a6
farman <farman@kmindz.in>
2022-12-03 19:37:16 +0530

    fixed

--- frontend/src/utils/url.js
+++ frontend/src/utils/url.js
@@ -1,5 +1,6 @@
 
 
-export const url = "https://proecommerceshop.herokuapp.com/"
+// export const url = "https://proecommerceshop.herokuapp.com/"
+export const url = "https://e-commerce-phi-plum.vercel.app/"
 
 // export const url = "http://localhost:8000/" 
